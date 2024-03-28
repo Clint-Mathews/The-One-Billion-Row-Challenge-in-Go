0fc40d1e584084bc84f253cfccbc1256d09ed3e2
Clint-Mathews <mathewsclint28@gmail.com>
2024-03-28 19:18:10 +0530

    INITIAL COMMIT

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Clint-Mathews/The-One-Billion-Row-Challenge-in-Go
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("One Billion Row Challenge in Go")
+}
