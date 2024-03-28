8e02ad087a30c3d752030d706882614c9c8cfb67
Clint-Mathews <mathewsclint28@gmail.com>
2024-03-28 20:46:43 +0530

    ADDED PROFILING FOR THE PROGRAM

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"runtime/trace"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+var executionprofile = flag.String("execprofile", "", "write tarce execution to `file`")
 
 func main() {
+	flag.Parse()
+
+	if *executionprofile != "" {
+		f, err := os.Create(*executionprofile)
+		if err != nil {
+			log.Fatal("could not create trace execution profile: ", err)
+		}
+		defer f.Close()
+		trace.Start(f)
+		defer trace.Stop()
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	executeLogic()
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+}
+
+func executeLogic() {
+	// Main execution
 	fmt.Println("One Billion Row Challenge in Go")
+
 }
